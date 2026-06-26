64dbfa62a0c7fb857990051ae3547e73489f626c
Damon Harvey <damonjamesharvey@gmail.com>
2026-06-26 02:29:35 -0500

    Added click speed widget.

--- AutoClicker.py
+++ AutoClicker.py
@@ -22,13 +22,13 @@ class AutoClicker(QMainWindow):
         super().__init__()
 
         self.init_coordinates_widget()
+        self.init_clicker_widget()
         self.place_widgets()
 
     def init_coordinates_widget(self):
         layout = QGridLayout()
         container = QWidget()
         container.setLayout(layout)
-
         self.coordinates_widget = container
 
         watcher = ClickWatcher()
@@ -73,6 +73,41 @@ class AutoClicker(QMainWindow):
         layout.addWidget(y_value_box, 0, 3)
         layout.addWidget(button, 1, 0, 1, 4)
 
+    def init_clicker_widget(self):
+        layout = QGridLayout()
+        container = QWidget()
+        container.setLayout(layout)
+        self.clicker_widget = container
+
+        def on_click():
+
+            for _ in range(100):
+                pyautogui.click(self.click_position.x(), self.click_position.y())
+
+        def set_click_speed(speed: int):
+
+            pyautogui.PAUSE = 1 / speed
+
+        click_speed_label = QLabel("/cps")
+
+        validator = QIntValidator(0, 999)
+
+        speed_value_box = QLineEdit()
+        speed_value_box.setText("10")
+        speed_value_box.setFixedWidth(45)
+        speed_value_box.setMaxLength(3)
+        speed_value_box.setValidator(validator)
+        speed_value_box.textChanged.connect(
+            lambda: set_click_speed(int(speed_value_box.text()))
+        )
+
+        click_button = QPushButton("click")
+        click_button.clicked.connect(on_click)
+
+        layout.addWidget(speed_value_box, 0, 0)
+        layout.addWidget(click_speed_label, 0, 1)
+        layout.addWidget(click_button, 1, 0, 1, 2)
+
     def place_widgets(self):
 
         layout = QGridLayout()
@@ -80,7 +115,8 @@ class AutoClicker(QMainWindow):
         container.setLayout(layout)
         self.setCentralWidget(container)
 
-        layout.addWidget(self.coordinates_widget, 0, 0, Qt.AlignmentFlag.AlignCenter)
+        layout.addWidget(self.coordinates_widget, 0, 0)
+        layout.addWidget(self.clicker_widget, 0, 1)
 
 
 def main():
